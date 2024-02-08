63a445a84277aa10c1ff371505320919f9046bd1
Andrew <deyneka2003@gmail.com>
2024-02-08 20:33:56 +0200

    fix adrk Mode btn className

--- src/components/ButtonDarkMode/ButtonDarkMode.js
+++ src/components/ButtonDarkMode/ButtonDarkMode.js
@@ -40,7 +40,7 @@ const ButtonDarkMode = () => {
   const btnActive = 'dark-mode-btn dark-mode-btn--active';
 
   return (
-    <button lassName={darkMode === 'dark' ? btnActive : btnNormal} onClick={toggleDarkMode}>
+    <button className={darkMode === 'dark' ? btnActive : btnNormal} onClick={toggleDarkMode}>
       <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
       <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
     </button>
